c7a1cd4077e1527c6937807e7a3615dbe946fbc1
Breno Souza <156030544+brenojose@users.noreply.github.com>
2024-05-17 23:30:30 -0400

    Add files via upload
    Files for assignment assessment week 1 and 2, Web Development Project.

---
+++ Week 1 and 2/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>week 2</title>
+</head>
+<body>
+
+<?php echo 'Hello'; ?>
+<?php echo '<h1>Welcome to  week 2</h1>'; ?>
+<?php echo '<p>Let\'s try this backslash</p>'; ?>
+
+<h1> Welcome to HTTP 5225</h1>
+<p>we will learn PhP in this class</p>
+<p>Testing paragraph</p>
+
+<?php echo
+'<ul> 
+    <li>PHP</li>
+    <li>mySQL</li>
+    <li>Laravel</li> 
+</ul>';
+?>
+
+<?php echo '<img src="https://s1.1zoom.me/big0/73/Mandarine_Wicker_basket_618880_1280x720.jpg" alt="Placeholder image" width="1280" height="720">'; ?>   
+
+ <?php
+ $fname = 'Breno';
+ $lname = 'Souza';
+ $name['fname'] = 'Breno';
+//$name = array ['Breno' . 'Souza'];
+?>
+
+<?php 
+echo '<br>';
+echo 'Hello ' . $fname . ' ' . $lname;
+?>
+
+</body>
+</html>
---
+++ Week 1 and 2/week1_2_assessment_brenosouza.php
@@ -0,0 +1,28 @@
+/*
+Hi, Gaurav!
+
+Since I don't know what exactly we're supposed to write down in the Week1&2 Assessment, I'll assume that I have to tell what I learned from those two weeks!
+So, let's get going!
+
+- Use of echo
+    In this second class you taught us how to use the 'echo' statement to output data to the browser, fundamentally
+    displaying information and debugging our code. You also taught us about the use of backslash to escape the apostrophe ('\')
+    so it won't confuse the code at all.
+
+        echo 'let\'s read this assessment!';
+
+- Server Side vs. Client side
+    Throughout the lecture you showed us the difference between server and client side, and what I could understand about this was that Client-side refers to actions that occur on the user's device, such as their computer or browser. Server-side refers to actions that occur on the web server. Although I always played online and offline games and tried to hack them since I was a kid, so I have an understanding about the principles of server and client side.
+
+- Setting up the php files using MAMP
+    As a Windows user I've learned to use and setup the php functionality using the htdocs file inside the MAMP folder, so it can work with myphp.
+
+- Variables and Array
+Reading the .pptx I've learned how to define and manipulate both indexed and associative arrays. This included understanding how to store and access different types of data within arrays.
+
+- Sessions and Cookies 
+Even reading the file provided seems not to be sufficient to understand quite well the content about this subject in my opinion, so I'll search for another sources to be ready for the next week's class!
+
+Overall, I think I absorbed most of the content shown in those weeks, thank you for the classes, professor!
+*/
+
